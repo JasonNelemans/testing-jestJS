25d733f70a97da26877d57c396fcb866d0783ee7
JasonNelemans <jasonnelemans@hotmail.com>
2020-06-09 10:58:42 +0200

    fresh start

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import "./App.css";
+
+import { Switch, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Loading from "./components/Loading";
+import MessageBox from "./components/MessageBox";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import MyHomepage from "./pages/MyHomepage";
+import Homepages from "./pages/Homepages";
+import HomepageDetails from "./pages/HomepageDetails";
+
+import { useDispatch, useSelector } from "react-redux";
+import { selectAppLoading } from "./store/appState/selectors";
+import { getUserWithStoredToken } from "./store/user/actions";
+
+function App() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectAppLoading);
+
+  useEffect(() => {
+    dispatch(getUserWithStoredToken());
+  }, [dispatch]);
+
+  return (
+    <div className="App">
+      <Navigation />
+      <MessageBox />
+      <Switch>
+        {isLoading ? <Loading /> : null}
+        <Route exact path="/" component={Homepages} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/login" component={Login} />
+        <Route path="/myhomepage" component={MyHomepage} />
+        <Route path="/homepages/:id" component={HomepageDetails} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
